d3ae5245fb6df2aac48df254b1a03c78162d06f0
Susan Traynor <105241393+traynosm@users.noreply.github.com>
2023-07-27 19:27:15 +0100

    Update greeting.py

--- greeting.py
+++ greeting.py
@@ -9,4 +9,4 @@ def helloall(): # different method name
  name = request.args.get('name')#retrieve GET parameters
  return("Hello {}!".format(name))#Python’s string.format
 if __name__ == "__main__":
- app.run(host='0.0.0.0',port='8080', ssl_context=('cert.pem', 'privkey.pem')) #Run the flask app at port 8080
+ app.run(host='0.0.0.0',port='8080', ssl_context=('fullchain.pem', 'privkey.pem')) #Run the flask app at port 8080
